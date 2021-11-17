2969a2b359adb0ab0f20248ff8bacf755fa53fb9
Max Lutsenko <makc199710@me.com>
2021-11-17 21:04:33 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -6,13 +6,56 @@ import DisplayComponent from "./components/DisplayComponent";
 function App() {
 
   const[time, setTime] = useState({ms:0, s:0, m:0, h:0})
+  const[interv, setInterv] = useState()
+  const[status, setStatus] = useState(0)
+
+  const start = () => {
+    run();
+    setStatus(1)
+    setInterv(setInterval(run,10))
+  }
+
+  const stop = () => {
+    clearInterval(interv)
+    setStatus(2)
+  }
+
+  const reset = () => {
+    clearInterval(interv)
+    setStatus(0)
+    setTime({ms:0, s:0, m:0, h:0})
+  }
+
+  const resume = () => start()
+
+  let updateMS = time.ms, updateS = time.s, updateM = time.m, updateH = time.h;
+
+  const run = () => {
+    if(updateH === 24){
+      updateH = 0;
+    }
+    if(updateM === 60){
+      updateH++;
+      updateM = 0;
+    }
+    if(updateS === 60){
+      updateM++;
+      updateS = 0;
+    }
+    if(updateMS === 100){
+      updateS++;
+      updateMS = 0;
+    }
+    updateMS++
+    return setTime({ms:updateMS, s:updateS, m:updateM, h:updateH})
+  }
 
   return (
     <div className="main-section">
       <div className="clock-holder">
         <div className="stopwatch">
           <DisplayComponent time={time}/>
-          <BtnComponent/>
+          <BtnComponent status={status} stop={stop} reset={reset} resume={resume} start={start}/>
         </div>
       </div>
     </div>
